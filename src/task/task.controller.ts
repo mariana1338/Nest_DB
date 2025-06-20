4decb8d32ecb1a5d1e3c8830844d7ef47f41d478
mariana1338 <mariprinces1338@gmail.com>
2025-06-20 17:42:35 -0500

    Refactor task service and controller to implement CRUD operations with proper DTOs and error handling

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { Task } from './entitys/task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateStatusTaskDto } from './dto/update-status-task.dto';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+  @Get()
+  findAll(): Promise<Task[]> {
+    return this.taskService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.taskService.findOne(id);
+  }
+  @Post()
+  create(@Body() body: CreateTaskDto): Promise<Task> {
+    return this.taskService.create(body);
+  }
+  @Put(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() body: CreateTaskDto): Promise<Task> {
+    return this.taskService.update(id, body);
+  }
+
+  @Put('status/:id')
+  updateStatus(
+    @Param('id',ParseIntPipe) id: number, 
+    @Body() body: UpdateStatusTaskDto
+  ): Promise<Task> {
+    return this.taskService.updateStatus(id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.taskService.delete(id);
+  }
 }
+
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,6 +2,8 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './entitys/task.entity';
 import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateStatusTaskDto } from './dto/update-status-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -20,12 +22,12 @@ export class TaskService {
     }
     return task;
   }
-  async create(task: Task): Promise<Task> {
+  async create(task: CreateTaskDto): Promise<Task> {
     const newTask = this.taskRepository.create(task);
     return this.taskRepository.save(newTask);
   }
 
-  async update(id: number, task: Task): Promise<Task> {
+  async update(id: number, task: CreateTaskDto): Promise<Task> {
     const existingTask = await this.taskRepository.findOneBy({ id });
     if (!existingTask) {
       throw new HttpException(
@@ -33,8 +35,29 @@ export class TaskService {
         HttpStatus.NOT_FOUND,
       );
     }
+    const updateTask = Object.assign(existingTask, task);
+    return this.taskRepository.save(updateTask);
+  }
 
-    const updatedTask = Object.assign(existingTask, task);
-    return this.taskRepository.save(updatedTask);
+  async updateStatus(id: number, status: UpdateStatusTaskDto): Promise<Task> {
+    const existingTask = await this.taskRepository.findOneBy({ id });
+    if (!existingTask) {
+      throw new HttpException(
+        `Task with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    existingTask.status = status.status;
+    return this.taskRepository.save(existingTask);
+  }
+
+  async delete(id: number): Promise<void> {
+    const result = await this.taskRepository.delete(id);
+    if (result.affected === 0) {
+      throw new HttpException(
+        `Task with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
