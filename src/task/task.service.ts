db7760610a6db234726e6cf4ffd1f59479706f38
mariana1338 <mariprinces1338@gmail.com>
2025-06-20 17:14:34 -0500

    Implement task service with CRUD operations and error handling

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entitys/task.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  @InjectRepository(Task)
+  private readonly taskRepository: Repository<Task>;
+  async findAll(): Promise<Task[]> {
+    return this.taskRepository.find();
+  }
+  async findOne(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOneBy({ id });
+    if (!task) {
+      throw new HttpException(
+        `Task with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return task;
+  }
+  async create(task: Task): Promise<Task> {
+    const newTask = this.taskRepository.create(task);
+    return this.taskRepository.save(newTask);
+  }
+
+  async update(id: number, task: Task): Promise<Task> {
+    const existingTask = await this.taskRepository.findOneBy({ id });
+    if (!existingTask) {
+      throw new HttpException(
+        `Task with ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const updatedTask = Object.assign(existingTask, task);
+    return this.taskRepository.save(updatedTask);
+  }
+}
